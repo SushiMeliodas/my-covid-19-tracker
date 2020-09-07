2b27c55426b6e10ef612f1e622ee75a44a6c39ff
Bryan Sim <bryanyf@hotmail.com>
2020-09-07 17:44:47 +0800

    Update all file

---
+++ src/Footer.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <div>
+      <div className="footer">
+        <div className="footer__top">
+          <div>
+            <h2>This is a Footer</h2>
+          </div>
+          <div>
+            <h2>This is a Footer</h2>
+          </div>
+          <div>
+            <h2>This is a Footer</h2>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
